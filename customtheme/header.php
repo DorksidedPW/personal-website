81f92afcc089487533a61590e72991151f37a3fb
PaulW <paulwassen@gmail.com>
2022-08-11 15:43:47 +0200

    Setting up basic wordpress template files

---
+++ customtheme/header.php
@@ -0,0 +1,9 @@
+<!Doctype html>
+<html lang='nl'>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Paul Wassen UI/UX design - Frontend developer</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
