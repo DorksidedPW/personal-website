d031eed7b7c3a9a878b59e0681a0ee48c71a3b41
Paul Wassen <paulwassen@gmail.com>
2022-08-13 16:32:52 +0200

    Enqueue custom stylesheet, disable woocommerce styles

---
+++ customtheme/functions.php
@@ -0,0 +1,14 @@
+<?php	
+    define("THEME_DIR", get_template_directory_uri());
+    
+    function enqueue_styles() {
+     
+        /** REGISTER css/screen.cs **/
+        wp_register_style( 'style.css', THEME_DIR . '/style.css', array(), '1', 'all' );
+        wp_enqueue_style( 'style.css' );
+             
+    }
+    add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
+    add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+?>
