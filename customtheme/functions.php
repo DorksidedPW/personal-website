198d14153cbdda3b2749d4f3243b3e27fb257140
PaulW <paulwassen@gmail.com>
2022-08-18 15:41:41 +0200

    Enqueued layout.js file

--- customtheme/functions.php
+++ customtheme/functions.php
@@ -4,6 +4,9 @@
     //Enqueue custom style.css
     function enqueue_styles() {
      
+        wp_register_script('layout.js', THEME_DIR . '/js/layout.js');
+        wp_enqueue_script( 'layout.js' );
+        
         /** REGISTER css/screen.cs **/
         wp_register_style( 'style.css', THEME_DIR . '/style.css', array(), '1', 'all' );
         wp_enqueue_style( 'style.css' );
