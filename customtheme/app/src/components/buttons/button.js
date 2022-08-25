7d43d1ccfdb27721ab230e54b0c91b94b9882f36
PaulW <paulwassen@gmail.com>
2022-08-25 12:53:03 +0200

    Started work on button component

---
+++ customtheme/app/src/components/buttons/button.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import './button.css';
+
+const Button = (props) => {
+  const {label} = props;
+
+  const buttonClick = () => {}
+
+  return(
+    <button
+      onClick={buttonClick}
+      className='button-base primary'
+      tabindex="0"
+    >
+      {label}
+    </button>
+  )
+}
+
+export default Button; 
