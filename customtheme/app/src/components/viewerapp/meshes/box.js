277470720da9c4588a23010460fc6f3db52024b9
PaulW <paulwassen@gmail.com>
2022-08-25 16:53:38 +0200

    Added basic mouse control

--- customtheme/app/src/components/viewerapp/meshes/box.js
+++ customtheme/app/src/components/viewerapp/meshes/box.js
@@ -1,18 +1,21 @@
-import React, { useRef, useState } from 'react'
-import { useFrame } from '@react-three/fiber'
+import React, { useRef, useState, useEffect } from 'react'
+import { useFrame, useThree } from '@react-three/fiber'
 
 const Box = (props) => {
   const mesh = useRef();
+  const { viewport } = useThree()
   const [active, setActive] = useState(false);
 
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  useFrame(({ mouse }) => {
+    if(active) {
+      const x = (mouse.x * viewport.width) / 2;
+      const y = (mouse.y * viewport.height) / 2;
+      mesh.current.rotation.set(-y, x, 0);  
+    }
   });
 
-  console.log('BOX')
-
   return(
-    <mesh position={[0, 0, -1]} ref={mesh}>
+    <mesh position={[0, 0, -1]} ref={mesh} onClick={(e) => setActive(!active)}>
       <boxGeometry args={[2, 2, 2]} rotation={[3, 1, 0]}/>
       <meshStandardMaterial />
     </mesh>
