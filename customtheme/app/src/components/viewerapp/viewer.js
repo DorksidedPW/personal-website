d923adbb32465e6aeaaadb5746cb99d099c9e620
PaulW <paulwassen@gmail.com>
2022-08-25 17:00:55 +0200

    Added scene states

--- customtheme/app/src/components/viewerapp/viewer.js
+++ customtheme/app/src/components/viewerapp/viewer.js
@@ -2,29 +2,27 @@ import { createRoot } from 'react-dom/client'
 import React, { useRef, useState, useEffect } from 'react'
 import Header from '../layout/header/header';
 import Box from './meshes/box'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 
 
 const Viewer = (props) => {
-
-  const[zoom, setZoom] = useState(45)
-
-  useEffect(() => {
-    setZoom(zoom)
-  },[zoom])
-
+  const[canvasProperties, setCanvasProperties] = useState({
+    zoom: 45,
+    intensity: 0.1
+  })
+  
   return(
     <div id='container' className='parallax'>
       <Header />
-      <div style={{display: 'flex', width: '100%', height: '100%'}}>
+      <div style={{display: 'flex', width: '100%', height: 'calc(100% - 80px'}}>
         <div style={{width: 400, height: '100%', backgroundColor: 'white'}}>
-          <button id='zoom-out' onClick={(e) => setZoom(zoom + 1)}>Zoom in</button>
-          <button id='zoom-in' onClick={(e) => setZoom(zoom - 1)}>Zoom in</button>
+          <button id='zoom-out'>Zoom in</button>
+          <button id='zoom-in'>Zoom in</button>
         </div>
         <div id="canvas-container" style={{width: '100vw', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f5f5f5'}}>
-          <Canvas camera={{ position: [0, 0, 5], fov: zoom}}>
-            <ambientLight intensity={0.1} />
-            <directionalLight color="red" position={[0, 0, 5]} />
+          <Canvas camera={{ position: [0, 0, 5], fov: canvasProperties.zoom}}>
+            <ambientLight intensity={canvasProperties.intensity} />
+            <directionalLight color="white" position={[0, 0, 5]} />
             <Box />
           </Canvas>
         </div>
