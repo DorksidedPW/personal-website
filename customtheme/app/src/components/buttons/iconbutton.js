97e149a1cf129499698baf6794aafbf3eb29c0f4
PaulW <paulwassen@gmail.com>
2022-08-25 13:39:48 +0200

    Removed unused elements from buttons

--- customtheme/app/src/components/buttons/iconbutton.js
+++ customtheme/app/src/components/buttons/iconbutton.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Icon from '@mdi/react'
 import './button.css';
 
 const IconButton = (props) => {
-  const{type, icon} = props;
-
-  let classNam
+  const{icon} = props;
 
   return(
     <button className='button-base icon-button'>
